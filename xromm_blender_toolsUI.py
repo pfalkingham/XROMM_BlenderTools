417db26e5664e0bc3042c3addc83b8643b715a83
Peter Falkingham <pfalkingham@googlemail.com>
2023-07-26 16:58:10 +0100

    Update xromm_blender_toolsUI.py
    updated UI - nothing's connected yet, but I add buttons and file pickers as the code is finished for those things

--- xromm_blender_toolsUI.py
+++ xromm_blender_toolsUI.py
@@ -3,7 +3,6 @@ import bpy
 ###########################################################
 #XCAM UI CODE
 ###########################################################
-
 # Define a file picker property
 bpy.types.Scene.maya_cam_file = bpy.props.StringProperty(
     name="",
@@ -29,6 +28,14 @@ bpy.types.Scene.image_sequence = bpy.props.BoolProperty(
     default=False
 )
 
+# Define a string property for the cameraname input box
+bpy.types.Scene.text_input = bpy.props.StringProperty(
+    name="Camera name",
+    description="xCam name",
+    default="xCam",
+    maxlen=1024
+)
+
 # Define an operator for creating an xCam
 class CreateXCamOperator(bpy.types.Operator):
     bl_idname = "scene.create_xcam"
@@ -48,8 +55,7 @@ class XCamPanel(bpy.types.Panel):
     bl_idname = "VIEW3D_PT_xcam"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = "XROMM"
-
+    bl_category = "XROMM"    
     def draw(self, context):
         layout = self.layout
         scene = context.scene
@@ -63,6 +69,13 @@ class XCamPanel(bpy.types.Panel):
         # Add the checkbox to the layout
         layout.prop(scene, "image_sequence")
         
+        # Add the text input box to the layout
+        row = layout.row()
+        row.label(text="Camera name:")
+        col = row.column()
+        col.prop(scene, "text_input", text="")
+
+        
         layout.operator("scene.create_xcam")
 
 
@@ -70,9 +83,31 @@ class XCamPanel(bpy.types.Panel):
 #IMPORT TRANSFORMATION UI CODE
 ###########################################################
 
+# Define a file picker property
+bpy.types.Scene.importfile = bpy.props.StringProperty(
+    name="",
+    description="Select a CSV file",
+    default="",
+    maxlen=1024,
+    subtype='FILE_PATH'
+)
+
+# Define an operator for creating an xCam
+class ImportOperator(bpy.types.Operator):
+    bl_idname = "scene.importfile"
+    bl_label = "Import Data"
+    bl_description = "Import data"
+
+    def execute(self, context):
+        ###########################################################
+        # TODO: Add logic here
+        ###########################################################
+        self.report({'INFO'}, "importing csv")
+        return {'FINISHED'}
+
 # Define a panel class
 class ImportPanel(bpy.types.Panel):
-    bl_label = "Import"
+    bl_label = "import_file"
     bl_idname = "VIEW3D_PT_import"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
@@ -83,7 +118,159 @@ class ImportPanel(bpy.types.Panel):
         layout = self.layout
         scene = context.scene
 
-        layout.label(text = "not yet implemented")
+        layout.label(text="Select CSV File:")
+        layout.prop(scene, "importfile", text="CSV")
+        layout.label(text = "To selected object or new sphere(s)")
+        layout.operator("scene.importfile")
+
+
+###########################################################
+#MARKERS UI CODE
+###########################################################
+
+# Define a panel class
+class markersPanel(bpy.types.Panel):
+    bl_label = "Markers"
+    bl_idname = "VIEW3D_PT_markers"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "XROMM"
+    bl_options = {'DEFAULT_CLOSED'}
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+
+        layout.label(text = "not yet implemented:")
+        layout.label(text = "vAvg")
+        layout.label(text = "CTex")
+
+
+
+
+###########################################################
+#AXES AND ROTATIONS UI CODE
+###########################################################
+
+
+# Define a string property for the axisname input box
+bpy.types.Scene.axis_input = bpy.props.StringProperty(
+    name="Axis name",
+    description="axis name",
+    default="",
+    maxlen=1024
+)
+
+#define pointers for the objects I'll pass to oRel
+bpy.types.Scene.oRel_axes = bpy.props.PointerProperty(type=bpy.types.Object)
+bpy.types.Scene.prox_obj = bpy.props.PointerProperty(type=bpy.types.Object)
+bpy.types.Scene.dist_obj = bpy.props.PointerProperty(type=bpy.types.Object)
+
+
+
+
+# Define an operator for creating a axes without locator
+class CreateAxesWOOperator(bpy.types.Operator):
+    bl_idname = "scene.create_axes_wo"
+    bl_label = "Create Axes"
+    bl_description = "Create Axes WITHOUT locators"
+
+    def execute(self, context):
+        ###########################################################
+        # TODO: Add logic here
+        ###########################################################
+        self.report({'INFO'}, "Creating axes WITHOUT locators")
+        return {'FINISHED'}
+
+# Define an operator for creating a axes WITH locators
+class CreateAxesWOperator(bpy.types.Operator):
+    bl_idname = "scene.create_axes_with"
+    bl_label = "Create Axes with locators"
+    bl_description = "Create Axes with locators"
+
+    def execute(self, context):
+        ###########################################################
+        # TODO: Add logic here
+        ###########################################################
+        self.report({'INFO'}, "Creating axes with locators")
+        return {'FINISHED'}
+
+
+
+# Define a panel class
+class axesPanel(bpy.types.Panel):
+    bl_label = "Axes and Rel Motion"
+    bl_idname = "VIEW3D_PT_axes"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "XROMM"
+    bl_options = {'DEFAULT_CLOSED'}
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+
+        layout.label(text = "Create Axes:")
+        row = layout.row()
+        row.label(text="Axis name:")
+        col = row.column()
+        col.prop(scene, "axis_input", text="")
+        row = layout.row()
+        row.operator("scene.create_axes_wo", text="Without locators")
+        row.operator("scene.create_axes_with", text="With locators")
+        
+        layout.separator()
+        
+        layout.label(text = "Output relative Motion:")
+        # Add two columns with object selectors
+        layout.prop_search(scene, "oRel_axes", scene, "objects", text="Axes")
+        layout.prop_search(scene, "prox_obj", scene, "objects", text="Proximal Object")
+        layout.prop_search(scene, "dist_obj", scene, "objects", text="Distal Object")
+        
+        # Add a button to call the oRel script
+        layout.separator()
+        layout.operator("scene.calculate_relative_motion", text="Calculate relative motion")
+
+# Define an operator for calculating relative motion
+class CalculateRelativeMotionOperator(bpy.types.Operator):
+    bl_idname = "scene.calculate_relative_motion"
+    bl_label = "Calculate relative motion"
+    bl_description = "Calculate relative motion"
+
+    def execute(self, context):
+        # Get the selected objects from the object selectors
+        axis_object = context.scene.axis_object
+        proximal_object = context.scene.proximal_object
+        distal_object = context.scene.distal_object
+        
+        # Call the oRel script with the selected objects as arguments
+        # TODO: Add logic here
+        self.report({'INFO'}, "Calculating relative motion")
+        return {'FINISHED'}
+
+
+
+###########################################################
+#Export UI CODE
+###########################################################
+
+# Define a panel class
+class exportPanel(bpy.types.Panel):
+    bl_label = "Export data"
+    bl_idname = "VIEW3D_PT_exp"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "XROMM"
+    bl_options = {'DEFAULT_CLOSED'}
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        layout.label(text = "not yet implemented:")
+        layout.label(text = "Export selected object(s):")
+        layout.label(text = "Translation | Rotation | Trans/Rot")
+
+
 
 
 
@@ -92,7 +279,7 @@ class ImportPanel(bpy.types.Panel):
 ###########################################################
 
 # Register the classes
-classes = (CreateXCamOperator, XCamPanel, ImportPanel)
+classes = (CreateXCamOperator, XCamPanel, ImportPanel, markersPanel, axesPanel, exportPanel,CreateAxesWOOperator,CreateAxesWOperator,CalculateRelativeMotionOperator,ImportOperator)
 
 def register():
     for cls in classes:
